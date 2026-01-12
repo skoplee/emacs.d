4d85acfd4556151fb20c8a2c8cf611942e16b45d
skoplee <ducker.liuu@gmail.com>
2026-01-12 23:45:06 +0800

    fix: 调整custom.el

--- lisp/custom.el
+++ lisp/custom.el
@@ -4,14 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-display-line-numbers-mode t)
- '(package-selected-packages
-   '(company doom-modeline embark-consult evil evil-anzu evil-collection
-	     evil-matchit evil-nerd-commenter evil-snipe evil-surround
-	     gruvbox-theme keycast marginalia orderless org-contrib
-	     quickrun restart-emacs undo-tree vertico yasnippet
-	     yasnippet-snippets))
- '(tool-bar-mode nil))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
